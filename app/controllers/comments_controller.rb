5300b1ae0f7bd128ca685f8dd441f948c01f4e62
Denis Zablotsky <dZablotsky@yandex.ru>
2019-08-07 22:19:12 +0600

    add relations to post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,13 @@
 class CommentsConroller < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
 
+  private
+
+  def comment_params
+    prarms.require(:comment).permit(:content)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_155709) do
+ActiveRecord::Schema.define(version: 2019_08_07_160113) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
